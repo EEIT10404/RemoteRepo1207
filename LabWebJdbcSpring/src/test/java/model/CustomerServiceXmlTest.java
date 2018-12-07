3cd72737a47cb3d5db74dee9605ddce42ed036bd
曾昱潔 <jasminetseng0427@gmail.com>
2018-12-07 14:25:49 +0800

    第一次commit

---
+++ LabWebJdbcSpring/src/test/java/model/CustomerServiceXmlTest.java
@@ -0,0 +1,45 @@
+package model;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CustomerServiceXmlTest {
+	
+	private static ApplicationContext context;
+	@BeforeClass
+	public static void beforeClass() {
+		context = new ClassPathXmlApplicationContext("beans.config.xml");
+	}
+	
+	private CustomerService customerService;
+	@Before
+	public void before() {
+		customerService = (CustomerService) context.getBean("customerService");
+	}
+
+	@Test
+	public void testLogin() {
+		CustomerBean alex = customerService.login("Alex", "A");
+		Assert.assertNotNull(alex);
+		
+		CustomerBean login = customerService.login("Alex", "AAA");
+		Assert.assertNull(login);
+	}
+	
+	@Test
+	public void testChangePassword() {
+		boolean change = customerService.changePassword("Ellen", "EEE", "EEE");
+		Assert.assertTrue(change);
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		((ConfigurableApplicationContext) context).close();
+	}
+}
---
+++ LabWebJdbcSpring/src/test/java/model/ProductServiceJavaConfigTest.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ProductServiceJavaConfigTest {
+	private static ApplicationContext context;
+	@BeforeClass
+	public static void beforeClass() {
+		context = new AnnotationConfigApplicationContext(
+				configuration.SpringJavaConfiguration.class);
+	}
+	private ProductService productService;
+	@Before
+	public void before() {
+		productService = (ProductService) context.getBean("productService");
+	}
+	
+	@Test
+	public void testSelect() {
+		List<ProductBean> beans = productService.select(null);
+		Assert.assertNotNull(beans);
+		Assert.assertTrue(beans.size()>1);
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		((ConfigurableApplicationContext) context).close();
+	}
+}
